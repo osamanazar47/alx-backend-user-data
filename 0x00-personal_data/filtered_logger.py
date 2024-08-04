d1594a73176b6e4fb96b09cb9edc6cb5c5f030b3
osamanazar47 <osamanazar47@gmail.com>
2024-08-04 19:54:59 +0300

    created the files and done with the first task (creating the filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,10 @@
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """for the filtering a log message"""
     pattern = '|'.join([f'{field}=[^{separator}]+'for field in fields])
-    return re.sub(pattern, lambda match: match.group(0).split('=')[0] + '=' + redaction, message)
+    return re.sub(pattern, lambda match: match.group(0).split('=')[0] +
+                  '=' + redaction, message)
