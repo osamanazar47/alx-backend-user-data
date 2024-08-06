205006fc5a47c035b47f56a6ce3f06252bf79576
osamanazar47 <osamanazar47@gmail.com>
2024-08-06 12:46:47 +0300

    created auth.py file and created class auth with some unfinished methods

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for the authentication in the app"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Defining class Auth for app authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns a boolean"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """checks the authorization in the header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None for now"""
+        return None
