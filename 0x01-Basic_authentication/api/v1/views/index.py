a17d223373158cf22c0e715579a22f13913d9047
osamanazar47 <osamanazar47@gmail.com>
2024-08-06 12:07:30 +0300

    added an error handler and a test route for it

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def authorization() -> str:
+  """raises a 401 error"""
+  abort(401)
