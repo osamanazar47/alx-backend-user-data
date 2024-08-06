266dcf75dc5e4524de68cab3effbe9ddaf68127a
osamanazar47 <osamanazar47@gmail.com>
2024-08-06 12:56:21 +0300

    done with require_path method in the Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,20 @@ class Auth:
     """Defining class Auth for app authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns a boolean"""
-        return False
+        if path is None:
+            return True
+        
+        # Check if excluded_paths is None or empty
+        if not excluded_paths:
+            return True
+
+        # Check if the path is in excluded_paths
+        path_with_slash = path if path.endswith('/') else path + '/'
+        for excluded_path in excluded_paths:
+            if path_with_slash.startswith(excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """checks the authorization in the header"""
