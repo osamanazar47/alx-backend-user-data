625972d309a70d1890b582e383b2cc95d4185abb
osamanazar47 <osamanazar47@gmail.com>
2024-08-08 12:59:08 +0300

    task 7 created extract base64 from header function in the basic auth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,12 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """class for authentication using basic auth"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Returns Base64 part of the Authorization header"""
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
