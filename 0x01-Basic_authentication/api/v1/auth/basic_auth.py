19a21ad00fda5abc49d4f5a70e68035554f5c90d
osamanazar47 <osamanazar47@gmail.com>
2024-08-08 13:19:10 +0300

    task 9 : function for extracting the credetials from the auth header style fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,7 +34,9 @@ class BasicAuth(Auth):
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str,
+                                 ) -> (str, str):
         """Returns the user email and password from the Base64 decoded value"""
         if decoded_base64_authorization_header is None:
             return None, None
@@ -42,5 +44,5 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
-        return user_email, user_password
+        u_email, u_password = decoded_base64_authorization_header.split(':', 1)
+        return u_email, u_password
