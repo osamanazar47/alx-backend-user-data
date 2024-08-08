e68cb1a6d3bae8b0c696a8de21099815624d23c8
osamanazar47 <osamanazar47@gmail.com>
2024-08-08 12:51:45 +0300

    task 6 created the BasicAuth class but empty till now style fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module for the authentication in the app using basic auth"""
+"""Module for the authentication in the app using
+basic auth"""
 from flask import request
 from typing import List, TypeVar
 from .auth import Auth
@@ -7,4 +8,4 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """class for authentication using basic auth"""
-    
+    pass
