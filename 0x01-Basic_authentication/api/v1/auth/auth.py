4fc04e7146b123abd0b3ec479c444a7eeb2393fb
osamanazar47 <osamanazar47@gmail.com>
2024-08-08 12:43:24 +0300

    task 5: the before request function and the auth header function done

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """checks the authorization in the header"""
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None for now"""
