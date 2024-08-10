3717eec65fb5aa8030b195201b5959b3bd484439
osamanazar47 <osamanazar47@gmail.com>
2024-08-10 12:17:16 +0300

    task2 : created method create_session in the SessionAuth class for creating sessions

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,8 +6,18 @@ from typing import List, TypeVar
 from .auth import Auth
 import base64
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """Defining the class for Session auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """method for creating the session object that
+        stores the session info"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
