cb725a23a9efbc33a38c1b11f6d8db550a8d8ada
osamanazar47 <osamanazar47@gmail.com>
2024-08-10 12:05:05 +0300

    task 1 created an empty SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for the authentication in the app using
+session authentication"""
+from flask import request
+from typing import List, TypeVar
+from .auth import Auth
+import base64
+from models.user import User
+
+
+class SessionAuth(Auth):
+    pass
