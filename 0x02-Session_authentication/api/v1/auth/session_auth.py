05ccf158488a562da616ad29914cf0bdacda8dfa
osamanazar47 <osamanazar47@gmail.com>
2024-08-11 12:37:16 +0300

    task 8: for logout route and destroy session function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,7 +31,7 @@ class SessionAuth(Auth):
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns a User instance based on cookie value"""
         session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(session_cookie)
+        user_id = self.user_id_for_session_id(session_id)
         user = User.get(user_id)
         return user
 
