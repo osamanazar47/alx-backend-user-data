fa8d805603f61c74533b8ffaba9a99a50076850f
osamanazar47 <osamanazar47@gmail.com>
2024-08-13 16:09:09 +0300

    created hash_password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for the authentication"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """for hashing password using bcrypt"""
+    password = password.encode('utf-8')
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed
