2fb169aea14980796a98aa2c9ec8c260d2f8b538
osamanazar47 <osamanazar47@gmail.com>
2024-08-14 13:49:22 +0300

    created register method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 """module for the authentication"""
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user to the db"""
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f'User {email} already exists')
+        else:
+            hashed_pass = _hash_password(password)
+            return self._db.add_user(email=email, hashed_password=hashed_pass)
 
 
 def _hash_password(password: str) -> bytes:
