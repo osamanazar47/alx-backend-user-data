ee2f4778bcb274a4a5d8d9c2965f1bb4c4d6f32c
osamanazar47 <osamanazar47@gmail.com>
2024-08-14 14:02:33 +0300

    created the flask app with the home route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for a basic flask app"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'],
+           strict_slashes=False)
+def home():
+    """for the home route"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
